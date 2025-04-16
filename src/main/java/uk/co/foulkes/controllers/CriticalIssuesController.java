11a2278926ca9fc0ecf735d2c1226c6905e9d076
Dan Foulkes <danfoulkes@gmail.com>
2025-04-16 22:14:34 +0100

    feat - code optimisations, fix tests and few other bits started around containerisation. Also improved unit tests.

--- src/main/java/uk/co/foulkes/controllers/CriticalIssuesController.java
+++ src/main/java/uk/co/foulkes/controllers/CriticalIssuesController.java
@@ -19,6 +19,7 @@ public class CriticalIssuesController {
       private static final String SEVERITY = "severity";
       private static final String FIRING = "firing";
       private static final String CRITICAL = "critical";
+
       @Autowired
       public CriticalIssuesController(ApplicationEventPublisher eventPublisher) {
           this.eventPublisher = eventPublisher;
@@ -26,23 +27,20 @@ public class CriticalIssuesController {
 
     @PostMapping("/critical-issues")
     public String criticalIssues(@RequestBody AlertManagerRequest alertManagerRequest) {
-        if (alertManagerRequest == null) {
-            return "No alert manager request provided";
-        }
-        if (alertManagerRequest.status() == null || alertManagerRequest.status().isEmpty()) {
-            return "No status provided in the alert manager request";
-        }
-        if (alertManagerRequest.commonLabels() == null || alertManagerRequest.commonLabels().isEmpty()) {
-            return "no common labels provided in the alert manager request";
-        }
-        if(alertManagerRequest.commonLabels().get(SEVERITY) == null || alertManagerRequest.commonLabels().get(SEVERITY).isEmpty()) {
-            return "no severity provided in the alert manager request";
-        }
-        if (CRITICAL.equalsIgnoreCase(alertManagerRequest.commonLabels().get(SEVERITY)) &&  FIRING.equalsIgnoreCase(alertManagerRequest.status())) {
+        if (preflightChecks(alertManagerRequest) && isCritical(alertManagerRequest)) {
             eventPublisher.publishEvent(new AlertManagerEvent(this, alertManagerRequest));
         }
-
         return "Critical Issues";
     }
 
+    private boolean preflightChecks(AlertManagerRequest alertManagerRequest) {
+        return alertManagerRequest != null && alertManagerRequest.status() != null
+                && !alertManagerRequest.status().isEmpty() && alertManagerRequest.commonLabels() != null
+                && !alertManagerRequest.commonLabels().isEmpty();
+    }
+
+    private boolean isCritical(AlertManagerRequest alertManagerRequest) {
+        return CRITICAL.equalsIgnoreCase(alertManagerRequest.commonLabels().get(SEVERITY))
+                && FIRING.equalsIgnoreCase(alertManagerRequest.status());
+    }
 }
